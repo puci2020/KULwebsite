d9424027040345c542c0cda56678597821cc2054
puci2020 <adam.pucicki97@gmail.com>
2020-01-18 16:57:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta charset="UTF-8"/>
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans&display=swap" rel="stylesheet">
+    <title>Zjazd absolwentów</title>
+</head>
+<body>
+
+<div class="first">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-sm-12">
+                <div class="name">
+                    <p class="one">ZJAZD ABSOLWENTÓW</br></p>
+                        <p class="two">PSYCHOLOGII</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <div class="form-box">
+                    <h1>Zapisz się</h1>
+                    <form>
+                        <label>Imię</label>
+                        <input type="text" name="firstName" id="firstName" placeholder="Podaj imię">
+                        <label>Nazwisko</label>
+                        <input type="text" name="lastName" id="lastName" placeholder="Podaj nazwisko">
+                        <label>E-mail</label>
+                        <input type="text" name="email" id="email" placeholder="Podaj email">
+
+                        <input type="submit" name="submit" id="submit" value="Zapisz">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-4 secound">
+            alskdaslkdj
+        </div>
+        <div class="col-sm-12 col-md-4 secound">
+            alskdaslkdj
+        </div>
+        <div class="col-sm-12 col-md-4 secound">
+            alskdaslkdj
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
