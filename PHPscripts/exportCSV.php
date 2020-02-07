74e96077439643178083c4c22f55287d6fbcc69e
puci2020 <adam.pucicki97@gmail.com>
2020-02-07 20:31:20 +0100

    add export table to CSV

--- PHPscripts/exportCSV.php
+++ PHPscripts/exportCSV.php
@@ -6,7 +6,7 @@ if(isset($_POST['export'])){
     header('Content-Disposition: attachment; filename=data.csv');
     $output = fopen("php://output", "w");
     fputcsv($output, array('ID', 'Imię', 'Nazwisko', 'Email', 'Rocznik', 'Dieta', 'Płatność'));
-    $sql = "select * from graduates";
+    $sql = "select * from graduates order by ID_user ASC";
     $result = $connect->query($sql);
     while ($row = mysqli_fetch_assoc($result)){
         fputcsv($output, $row);
--- PHPscripts/exportPDF.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "db_connection.php";
-
-if(isset($_POST['exportPDF'])){
-    require_once('tcpdf/tcpdf.php');
-    
-}
-
-?>
